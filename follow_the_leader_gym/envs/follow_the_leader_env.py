579efb9f6a3ef525b2eddd2a4a22ea1957a106ca
phnk <carl.borngrund@gmail.com>
2022-10-14 11:12:39 +0200

    changed the observation to always give a spike out, rather than allowing all beams to miss the target

--- follow_the_leader_gym/envs/follow_the_leader_env.py
+++ follow_the_leader_gym/envs/follow_the_leader_env.py
@@ -9,12 +9,12 @@ class FollowTheLeaderEnv(gym.Env):
     metadata = {"render_modes": ["human"], "render_fps": 4}
 
     # Main API
-    def __init__(self, n=2, render_mode=None):
+    def __init__(self, n=4, render_mode=None):
         self.MIN = 0 # min value of x
         self.MAX = 100 # max value of x
         self.SPEED = 1 # how fast we can move
         self.DISTANCE = 100
-        self.WINDOW_SIZE = 0.1
+        self.WINDOW_SIZE = 5
 
         assert n > 0
 
@@ -48,21 +48,25 @@ class FollowTheLeaderEnv(gym.Env):
         self.ax.set_ylim([0, 100])
 
         # caluclate the angle for our beams. Assumption: equally spaced out. We always have 1 beam straight forward.
-        incr = 90/(self.num_beams+1)
+        #incr = 90/(self.num_beams+1)
 
         lesser_angles = []
         for i in range(1, self.num_beams + 1):
-            lesser_angles.append(i * incr)
+            lesser_angles.insert(0, 90 - i * 3)
 
-        larger_angles = [x + 90 for x in lesser_angles.copy()]
+        self.lesser_angles = [math.radians(x) for x in lesser_angles]
 
-        self.rad_list = [math.radians(x) for x in lesser_angles + [90.0] + larger_angles]
+        larger_angles = []
+        for i in range(1, self.num_beams + 1):
+            larger_angles.append(90 + i * 3)
+
+        self.larger_angles = [math.radians(x) for x in larger_angles]
 
         obs = self._get_obs()
 
         info = self._get_info()
 
-        return obs, info
+        return obs
 
     def step(self, action):
         # process the action of the network
@@ -105,13 +109,31 @@ class FollowTheLeaderEnv(gym.Env):
     # Helpers
     def _get_obs(self):
         obs = [0] * (2 * self.num_beams + 1)
+        min_dist = 1e5
+        current_best_index = None
 
-        for i, rad in enumerate(self.rad_list):
-            beam_x = self.DISTANCE * math.tan(rad) + self.follower_x
+        for i, rad in enumerate(self.lesser_angles):
+            dist = self.DISTANCE / math.tan(rad)
+            beam_x_coord = self.follower_x - dist
 
-            if abs(self.leader_x - beam_x) < self.WINDOW_SIZE or (math.degrees(rad) == 90 and abs(self.leader_x - self.follower_x) < self.WINDOW_SIZE):
-                obs[i] = 1
-                break
+            if abs(self.leader_x - beam_x_coord) < min_dist:
+                min_dist = abs(self.leader_x - beam_x_coord)
+                current_best_index = i
+
+        for i, rad in enumerate(self.larger_angles):
+            dist = self.DISTANCE / math.tan(math.pi - rad)
+            beam_x_coord = self.follower_x + dist
+
+            if abs(self.leader_x - beam_x_coord) < min_dist:
+                min_dist = abs(self.leader_x - beam_x_coord)
+                current_best_index = i + len(self.lesser_angles) + 1
+
+        if len(obs) % 2 != 0 and abs(self.leader_x - self.follower_x) < min_dist:
+            current_best_index = math.ceil(len(obs) / 2) - 1
+
+        assert current_best_index is not None
+
+        obs[current_best_index] = 1
 
         assert len(obs) == 2*self.num_beams + 1
         return np.array(obs, dtype=np.int8)
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import gym
 import follow_the_leader_gym
 
 env = gym.make("Follow-The-Leader-v0")
-env.reset()
+obs = env.reset()
 
-for _ in range(1000):
+for _ in range(500):
     action = env.action_space.sample()
 
     obs, reward, done, info = env.step(action)
+    print(obs, reward)
     env.render()
 
 env.close()
